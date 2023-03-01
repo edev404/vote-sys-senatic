17f4f6ce5c2d111d3983546de96168bc5825a677
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-02-28 21:28:16 -0500

    Default project structure added

---
+++ vote-sys/src/test/java/com/senatic/votesys/VoteSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senatic.votesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

606c60de6eff3d5d661a2d1e507f10570fb5f5fc
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-03-01 13:24:49 -0500

    Update Perfil model
    Fix class content. Replace enum by class fields

--- vote-sys/src/main/java/com/senatic/votesys/model/Perfil.java
+++ vote-sys/src/main/java/com/senatic/votesys/model/Perfil.java
@@ -1,5 +1,26 @@
 package com.senatic.votesys.model;
 
-public enum Perfil {
-    
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="Perfiles")
+public class Perfil {
+
+    @Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String perfil;
 }

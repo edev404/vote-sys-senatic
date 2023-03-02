2137be30fc6b274d63507fde5ebd569f9a04b974
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-03-01 21:37:26 -0500

    Update votacionesServiceJpa and IVotacionesService
    Add the declaration and implementation to getting the votaciones paginate and both paginate with search by example

--- vote-sys/src/main/java/com/senatic/votesys/repository/VotacionesServiceJpa.java
+++ vote-sys/src/main/java/com/senatic/votesys/repository/VotacionesServiceJpa.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.senatic.votesys.model.Votacion;
@@ -30,6 +33,16 @@ public class VotacionesServiceJpa implements IVotacionesService{
     @Override
     public void deleteById(Long idVotacion) {
         votacionesJpa.deleteById(idVotacion);
+    }
+
+    @Override
+    public Page<Votacion> getVotacionesPaginate(Pageable paging) {
+        return votacionesJpa.findAll(paging);
+    }
+
+    @Override
+    public Page<Votacion> getVotacionesPaginateByExample(Pageable paging, Example<Votacion> example) {
+        return votacionesJpa.findAll(example, paging);
     }
     
 }
--- vote-sys/src/main/java/com/senatic/votesys/service/IVotacionesService.java
+++ vote-sys/src/main/java/com/senatic/votesys/service/IVotacionesService.java
@@ -2,10 +2,16 @@ package com.senatic.votesys.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.senatic.votesys.model.Votacion;
 
 public interface IVotacionesService {
     List<Votacion> getVotaciones();
     void addVotacion(Votacion votacion);
     void deleteById(Long idVotacion);
+    Page<Votacion> getVotacionesPaginate(Pageable paging);
+    Page<Votacion> getVotacionesPaginateByExample(Pageable paging, Example<Votacion> example);
 }

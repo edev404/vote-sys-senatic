898360a7c2077a22166f9044b966e695e22226d4
cristianPorrety <cristianporretydev@gmail.com>
2023-03-03 10:09:59 -0500

    done the first part or a demo of AprendicesController, the same with AprendicesJpa, adding some CRUD basic methods in the IAprendicesService and implemented in AprendicesJpa

--- vote-sys/src/main/java/com/senatic/votesys/controller/AprendicesController.java
+++ vote-sys/src/main/java/com/senatic/votesys/controller/AprendicesController.java
@@ -1,7 +1,15 @@
 package com.senatic.votesys.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +17,10 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.senatic.votesys.exception.CsvParsingException;
+import com.senatic.votesys.model.Aprendiz;
+import com.senatic.votesys.model.Votacion;
+import com.senatic.votesys.model.dto.AprendizDTO;
+import com.senatic.votesys.service.IAprendicesService;
 import com.senatic.votesys.utils.FileHandler;
 
 import ch.qos.logback.core.model.Model;
@@ -17,15 +29,23 @@ import ch.qos.logback.core.model.Model;
 @RequestMapping("/aprendices")
 public class AprendicesController {
     
-    @GetMapping("/create/form")
-    public String createAprendiz(){
+	@Autowired
+	private IAprendicesService aprendizService;
+	
+    @PostMapping("/create/form")
+    public String createAprendiz(AprendizDTO aprendiz, RedirectAttributes redirectAtt){
         /*
         TO DO: 
         Recibir un aprendiz del form y guardarlo mediante el servicio correspondiente
         Redirigir a la lista de aprendices
         Enviar mensaje de confirmación
          */
-        return "";
+    	if(!aprendizService.addAprendiz(null)) {
+    		redirectAtt.addFlashAttribute("msg", "el aprendiz ya existe");
+    	}else {
+    		redirectAtt.addFlashAttribute("msg", "el aprendiz fue almacenado satisfactoriamente");
+    	}
+        return "redirect:/aprendices/view";
     }
 
     @GetMapping("/create/upload")
@@ -57,43 +77,56 @@ public class AprendicesController {
     }
 
     @GetMapping("/view")
-    public String viewAprendices(Model model){
+    public String getAprendices(@RequestParam(defaultValue = "0") Integer page,
+            					@RequestParam(defaultValue = "5") Integer size,
+            					org.springframework.ui.Model model){
         //TO DO: Desplegar lista de aprendices en la vista
         /*
          Debe soportar paginación y ordenación por ficha
          Debe soportar busqueda By example por ficha
          */
-    	
-        return "";
+    	Pageable paging = PageRequest.of(page, size);
+        Page<Aprendiz> listAprendices = aprendizService.getAprendicesPaginate(paging);
+        model.addAttribute("aprendices", listAprendices);
+        return "admin/add-aprendiz/list";
     }
 
     @GetMapping("/delete/{id}")
-    public String deleteAprendizById(){
+    public String deleteAprendizById(@PathVariable("id") String id, RedirectAttributes redirectAtt){
         /*
         TO DO: 
         Eliminar al aprendiz por id y redirigir a la vista de la lista de aprendices
         Enviar mensaje de confirmación
          */
+    	aprendizService.deleteById(id);
+    	redirectAtt.addFlashAttribute("msg", "borrado exitosamente");
         return "redirect:/aprendices/view";
     }
 
     @GetMapping("/update/{id}")
-    public String updateAprendizView(){
+    public String updateAprendizView(@PathVariable("id") String id, org.springframework.ui.Model model){
         /*
         TO DO:
         Retornar el formulario de aprendiz con todos los datos correspondientes al aprendiz del id
          */
-        return "";
+    	model.addAttribute("aprendiz", aprendizService.findById(id).get());
+        return "admin/add-aprendiz/form";
     }
 
     @PostMapping("/update/")
-    public String updateAprendiz(){
+    public String updateAprendiz(AprendizDTO aprendiz, RedirectAttributes redirectAtt){
         /*
         TO DO:
         Recibir un aprendiz como parametro y almacenarlo
         Redirigir a la vista de aprendices
         Enviar mensaje de confirmación
          */
+    	aprendizService.updateAprendiz(aprendiz);
         return "redirect:/aprendices/view";
     }
+    
+    @InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+	}
 }
